597cfcab7607569ab26d2228e2a6b1dacea30ee0
Hugo <hugolacerdar@gmail.com>
2022-01-31 15:55:51 -0300

    refactor: complexity reduction

--- apps/micro/src/app/home/home.component.ts
+++ apps/micro/src/app/home/home.component.ts
@@ -12,23 +12,40 @@ export class HomeComponent {
   mode;
   widgets: Widget[];
 
-  updateWidgetsAndRecalculateCost(widget: Widget) {
-    switch (this.mode) {
-      case 'create':
-        const newWidget = Object.assign({}, widget, { id: uuidv4() });
-        this.widgets = [...this.widgets, newWidget];
-        break;
-      case 'update':
-        this.widgets = this.widgets.map(_widget => widget.id === _widget.id
-          ? Object.assign({}, widget) : _widget);
-        break;
-      case 'delete':
-        this.widgets = this.widgets.filter(_widget => widget.id !== _widget.id);
-        break;
-      default:
-        break;
-    }
-    return this.widgets.reduce((acc, curr) => acc + curr.price, 0);
+  reCalculateTotal(mode: string, widgets, newWidget: Widget) {
+    this.widgets = this.updateWidgets(mode, widgets, newWidget);
+    this.price = this.getTotalPrice(this.widgets);
   }
 
+  updateWidgets(mode: string, widgets, newWidget) {
+    switch (mode) {
+      case 'create':
+        return this.addWidget(widgets, newWidget);
+      case 'update':
+        return this.updateWidget(widgets, newWidget);
+      case 'delete':
+        return this.deleteWidget(widgets, newWidget);
+      default:
+        return widgets;
+    }
+  }
+
+  getTotalPrice(widgets) {
+    return widgets.reduce((acc, curr) => acc + curr.price, 0);
+  }
+
+  addWidget(widgets, widget) {
+    const newWidget = Object.assign({}, widget, { id: uuidv4() });
+    return [...widgets, newWidget];
+  }
+
+  updateWidget(widgets, widget) {
+    return widgets.map((_widget) =>
+      widget.id === _widget.id ? Object.assign({}, widget) : _widget
+    );
+  }
+
+  deleteWidget(widgets, widget) {
+    return widgets.filter((_widget) => widget.id !== _widget.id);
+  }
 }
